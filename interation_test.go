4dfc93674549408513a90a5e746839fee0fd8341
wrekone <wrekone@users.noreply.github.com>
2020-01-16 19:39:02 -0800

    include Example

--- interation_test.go
+++ interation_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 8)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", b.N)
 	}
+}
+
+func ExampleRepeat() {
+	qs := Repeat("q", 10)
+	fmt.Println(qs)
+	// Output "qqqqqqqqqq"
 }
